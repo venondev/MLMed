724980dcec56ee342bc2c79143f5a01c83b0a764
Philipp <phpi@gmx.de>
2022-07-05 19:57:13 +0200

    update testing

--- pytorch3dunet/unet3d/buildingblocks.py
+++ pytorch3dunet/unet3d/buildingblocks.py
@@ -314,12 +314,14 @@ class Decoder(nn.Module):
                                          padding=padding)
 
     def forward(self, encoder_features, x):
+        print("0xxx",x.size()) 
         if self.use_attention_gate:
             temp=self.attention_gate(encoder_features,x)
             encoder_features = self.upsampling(encoder_features=encoder_features, x=temp)
-        print("after_encoder_features",encoder_features.size())           
+        print("after_encoder_features",encoder_features.size())
+        print("1xxx",x.size())          
         x = self.upsampling(encoder_features=encoder_features, x=x)
-        print("xxx",x.size())
+        print("2xxx",x.size())
 
         
         if self.auto_encoder:
