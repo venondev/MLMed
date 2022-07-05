20fed2412877f778763e0f9304ea6fd48a3c7db1
Philipp <phpi@gmx.de>
2022-07-05 13:34:18 +0200

    update testing

--- scripts/resize.py
+++ scripts/resize.py
@@ -3,8 +3,29 @@ import os
 import h5py
 from torch import nn as nn
 import torch
+import numpy as np
+import scipy
 
-pool = nn.MaxPool3d((2, 2, 2))
+
+k=4
+kernel_row=(np.array(list(range(1,k))+list(range(k,0,-1)))+4)/(k+4)
+print(kernel_row)
+kernel_2d=[]
+for i in kernel_row:
+    kernel_2d.append(i*kernel_row)
+kernel_2d=np.asarray(kernel_2d)
+kernel_3d=[]
+for i in kernel_row:
+    kernel_3d.append(kernel_2d*i)
+
+kernel_3d=np.asarray(kernel_3d)
+
+
+def clean_data(m):
+        t=np.ones_like(m)
+        t[m<0.4]=0
+        t=scipy.ndimage.convolve(t,kernel_3d)
+        return t*m
 for dataset_type in ["train", "val"]:
 
     # Input Dir
@@ -25,8 +46,9 @@ for dataset_type in ["train", "val"]:
             max_val = raw.max()
 
             raw = (raw - min_val) / (max_val - min_val)
+            raw = clean_data(raw)
 
-            t = f"./data/{dataset_type}_half"
+            t = f"./data/{dataset_type}_clean"
             if not os.path.isdir(t):
                 os.mkdir(t)
 
