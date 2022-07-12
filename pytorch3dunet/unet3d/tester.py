1381c34ff47f5cb9a61bf10dff5b6fa2b5a1e775
Philipp <phpi@gmx.de>
2022-07-12 15:51:52 +0200

    update testing

--- pytorch3dunet/unet3d/tester.py
+++ pytorch3dunet/unet3d/tester.py
@@ -59,6 +59,7 @@ class PrecomputedTester():
             hjalmar_pred = self.load_hjalmar(file)
             philipp_pred = self.load_philipp(file)
             min_shape=np.minimum(np.minimum(hjalmar_pred.shape,philipp_pred.shape),label.shape)
+            print(min_shape)
             sum = np.zeros_like(min_shape,dtype='float64')
             print(sum.shape)
             label = label[0:min_shape[0],0:min_shape[1],0:min_shape[2]]
